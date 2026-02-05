0d4ee4d00e4ab2ba54cd4c7cbdd16ebcac32a688
pereiraitapema-spec <pereira.itapema@gmail.com>
2026-02-05 14:46:27 -0300

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./services/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
